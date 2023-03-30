4785ce80096722f9fa4fc94926db06480fb51507
Estudiante <estudiante@UCUXA60640.continental.edu.pe>
2023-03-29 16:49:28 -1000

    Hola

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+create database repositorio
+use repositorio
+
+
+create table Cancion
+(
+Id_cancion INT primary key not null,
+nombre_cancion nvarchar(20) not null,
+nombre_autor nvarchar (30)not null,
+nombre_cantante nvarchar(30)not null,
+nombre_album nvarchar(30)not null,
+annio int not null)
+insert into Cancion (Id_cancion,nombre_cancion,nombre_autor,nombre_cantante,nombre_album,annio)
+values(1,'Just the Way You Are','Bruno Mars','Bruno Mars','Doo-Wops & Hooligans', 2010)
+
+select * from Cancion
+
+insert into Cancion (Id_cancion,nombre_cancion,nombre_autor,nombre_cantante,nombre_album,annio)
+values(2,'Just the ','Bruno','Bruno ','Wops & Hoos', 2012)
